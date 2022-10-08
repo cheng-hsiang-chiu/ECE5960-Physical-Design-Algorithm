bb96c620456ac43125aca59b60b2d1e3882b9c93
chchiu <chchiu@pass-2.ece.utah.edu>
2022-10-07 23:53:32 -0600

    updated run.sh

--- PA2/run.sh
+++ PA2/run.sh
@@ -1,8 +1,18 @@
 #! /bin/bash
 
-for i in {0..6}
+files=('1' '2' '3' 'ami33' 'ami49' 'apte' 'hp' 'xerox')
+
+for i in ${files[@]}
 do
-  echo "input_"$i
-  time ./fm ../benchmark/input_$i.dat ./output_$i.dat
-  ../checker_linux ../benchmark/input_$i.dat ./output_$i.dat
+  echo "---------------------------------"
+  echo "-----------  "$i"   -------------"
+  echo "---------------------------------"
+  ./fp 0.6 "../benchmarks/"$i".block" "../benchmarks/"$i".nets" "./"$1".out"
 done
+
+#for i in {0..6}
+#do
+#  echo "input_"$i
+#  time ./fm ../benchmark/input_$i.dat ./output_$i.dat
+#  ../checker_linux ../benchmark/input_$i.dat ./output_$i.dat
+#done
