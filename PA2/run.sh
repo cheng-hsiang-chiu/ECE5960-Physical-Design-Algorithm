191d59fed9d2435b351206b7c057430f183e3506
chchiu <chchiu@pass-2.ece.utah.edu>
2022-10-12 22:38:07 -0600

    updated input file path

--- PA2/run.sh
+++ PA2/run.sh
@@ -7,12 +7,5 @@ do
   echo "---------------------------------"
   echo "-----------  "$i"   -------------"
   echo "---------------------------------"
-  time ./fp 0.6 "../benchmarks/"$i".block" "../benchmarks/"$i".nets" "./"$1".out"
+  time ./fp 0.6 "../input_pa2/"$i".block" "../input_pa2/"$i".nets" "./"$1".out"
 done
-
-#for i in {0..6}
-#do
-#  echo "input_"$i
-#  time ./fm ../benchmark/input_$i.dat ./output_$i.dat
-#  ../checker_linux ../benchmark/input_$i.dat ./output_$i.dat
-#done
